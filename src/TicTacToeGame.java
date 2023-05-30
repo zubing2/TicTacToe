aca4b89a4c0735532bbc725db776232366083ea0
Zubin Guan <zubing2@nycstudents.net>
2023-05-30 02:19:43 -0400

    initial

--- src/TicTacToeGame.java
+++ src/TicTacToeGame.java
@@ -1,2 +1,114 @@
-package PACKAGE_NAME;public class TicTacToeGame {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class TicTacToeGame extends JFrame implements ActionListener {
+    private JButton[][] buttons;
+    private boolean playerX;
+
+    public TicTacToeGame() {
+        setTitle("Tic Tac Toe");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setSize(300, 300);
+        setLayout(new GridLayout(3, 3));
+
+        buttons = new JButton[3][3];
+        playerX = true;
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                buttons[i][j] = new JButton();
+                buttons[i][j].setFont(new Font(Font.SANS_SERIF, Font.BOLD, 50));
+                buttons[i][j].addActionListener(this);
+                add(buttons[i][j]);
+            }
+        }
+
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(TicTacToeGame::new);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        JButton button = (JButton) e.getSource();
+
+        if (button.getText().isEmpty()) {
+            if (playerX) {
+                button.setText("X");
+            } else {
+                button.setText("O");
+            }
+
+            button.setEnabled(false);
+            playerX = !playerX;
+
+            if (checkForWin()) {
+                JOptionPane.showMessageDialog(this, (playerX ? "O" : "X") + " wins!");
+                resetGame();
+            } else if (isBoardFull()) {
+                JOptionPane.showMessageDialog(this, "It's a draw!");
+                resetGame();
+            }
+        }
+    }
+
+    private boolean checkForWin() {
+        // Check rows
+        for (int i = 0; i < 3; i++) {
+            if (!buttons[i][0].getText().isEmpty() &&
+                    buttons[i][0].getText().equals(buttons[i][1].getText()) &&
+                    buttons[i][1].getText().equals(buttons[i][2].getText())) {
+                return true;
+            }
+        }
+
+        // Check columns
+        for (int i = 0; i < 3; i++) {
+            if (!buttons[0][i].getText().isEmpty() &&
+                    buttons[0][i].getText().equals(buttons[1][i].getText()) &&
+                    buttons[1][i].getText().equals(buttons[2][i].getText())) {
+                return true;
+            }
+        }
+
+        // Check diagonals
+        if (!buttons[0][0].getText().isEmpty() &&
+                buttons[0][0].getText().equals(buttons[1][1].getText()) &&
+                buttons[1][1].getText().equals(buttons[2][2].getText())) {
+            return true;
+        }
+
+        if (!buttons[0][2].getText().isEmpty() &&
+                buttons[0][2].getText().equals(buttons[1][1].getText()) &&
+                buttons[1][1].getText().equals(buttons[2][0].getText())) {
+            return true;
+        }
+
+        return false;
+    }
+
+    private boolean isBoardFull() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (buttons[i][j].getText().isEmpty()) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private void resetGame() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                buttons[i][j].setText("");
+                buttons[i][j].setEnabled(true);
+            }
+        }
+        playerX = true;
+    }
 }
